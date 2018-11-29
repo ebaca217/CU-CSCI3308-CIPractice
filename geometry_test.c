74fa0121e95d8cbdb4bfc81bc1adcce35765d09a
user <elba6527@colorado.edu>
2018-11-28 18:43:38 -0700

    adding files

--- geometry_test.c
+++ geometry_test.c
@@ -91,6 +91,22 @@ START_TEST(test_2d_dist)
 }
 END_TEST
 
+START_TEST(test_2d_area_triangle)
+{
+    coord_2d_t a;
+    coord_2d_t b;
+    coord_2d_t c;
+    double exp;
+    double area;
+
+    a.x = b.x = c.x = 0;
+    a.y = b.y = c.y = 0;
+    area = coord_2d_area_triangle(&a, &b, &c);
+    exp = 0;
+    ck_assert(area == exp);
+}
+END_TEST
+
 /* coord_2d_midpoint Test */
 START_TEST(test_2d_midpoint)
 {
@@ -162,11 +178,14 @@ Suite* coord_2d_suite(void)
     TCase* tc_2d_midpoint = tcase_create("coord_2d_midpoint");
     tcase_add_test(tc_2d_midpoint, test_2d_midpoint);
 
+    TCase* tc_2d_area_triangle = tcase_create("coord_2d_area_triangle");
+    tcase_add_test(tc_2d_area_triangle, test_2d_area_triangle);
+
     /* Add Cases to Suite */
     suite_add_tcase(s, tc_2d_eq);
     suite_add_tcase(s, tc_2d_dist);
     suite_add_tcase(s, tc_2d_midpoint);
-
+    suite_add_tcase(s, tc_2d_area_triangle);
     /* Return Suite */
     return s;
 
